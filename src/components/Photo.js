30a2830f3c763e4c1655e6536af84b0d4a1129e2
badouine <nzbadouine@gmail.com>
2021-08-30 13:09:34 +0000

    image load

---
+++ src/components/Photo.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Picture from './Picture';
+
+class Photo extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <Picture/>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Photo;
---
+++ src/components/Picture.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import photo from './image/imageInSrc.jpg';
+
+const Photo = () => {
+    return <img src={photo} width={500} height={400}alt=""/>
+}
+
+export default Photo;
